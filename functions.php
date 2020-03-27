74d7e320e2b92481df6c34e2c88e9bdc803443ad
chathuranga99 <35645878+chathuranga99@users.noreply.github.com>
2020-03-28 01:54:06 +1030

    debug functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-fucntion load_style_sheets(){
+function load_style_sheets(){
 
 	wp_register_style('bootstrap_stylsheet',get_template_directory_uri().'/css/bootstrap.min.css', array(),false, 'all');
 	wp_enqueue_style('bootstrap_stylsheet');
