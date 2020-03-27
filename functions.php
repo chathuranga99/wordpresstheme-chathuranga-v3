585b8f6a3930138eb7f282b9833b81a80fc0c813
chathuranga99 <35645878+chathuranga99@users.noreply.github.com>
2020-03-28 02:22:35 +1030

    adding changes to functions.php 3

--- functions.php
+++ functions.php
@@ -4,5 +4,8 @@ function load_style_sheets(){
 
 	wp_register_style('bootstrap_stylsheet',get_template_directory_uri().'/css/bootstrap.min.css', array(),false, 'all');
 	wp_enqueue_style('bootstrap_stylsheet');
+
+	wp_register_style('custom_stylsheet',get_template_directory_uri().'/style.css', array(),false, 'all');
+	wp_enqueue_style('custom_stylsheet');
 }
 add_action('wp_enqueue_scripts','load_style_sheets');
