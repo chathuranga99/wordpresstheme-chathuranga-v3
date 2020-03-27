8f1eea14bc8961d85eb4ad2f111835a328537840
chathuranga99 <35645878+chathuranga99@users.noreply.github.com>
2020-03-28 01:48:21 +1030

    adding functions and queing styles

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+fucntion load_style_sheets(){
+
+	wp_register_style('bootstrap_stylsheet',get_template_directory_uri().'/css/bootstrap.min.css', array(),false, 'all');
+	wp_enqueue_style('bootstrap_stylsheet');
+}
